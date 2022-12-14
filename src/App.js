6e5ee0e500365d42e30556dbd4743caf660b8d05
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-12-15 00:26:09 +0600

    React Bootstrap CSS import

--- src/App.js
+++ src/App.js
@@ -37,14 +37,24 @@ function App() {
 
 
          {/*////////////////////// Header Section Start/////////////////////////*/}
-         <div className='header'>
+         <header className='header'>
             <h1 className='greetings'>Hello! Welcome to my portfolio</h1>
             <h1 className='name'>I'm <span className='my-name'>Towhidur Rahman</span></h1>
             <h1 className='my-designation'><Typewriter options={{  strings: ['MERN Stack Developer', 'UI/UX Designer'],  autoStart: true,  loop: true,  }}/></h1>
             <div className='social'> <a className='social-link' href="https://www.facebook.com/mdtowhidur.rahman.92"><FaFacebook></FaFacebook></a>{"  "}<a className='social-link' href="https://www.linkedin.com/in/md-towhidur-rahman-83ba78259/"><FaLinkedin></FaLinkedin></a>{"  "} <a className='social-link' href="https://github.com/Towhid47"><FaGithub></FaGithub></a></div>
             <div className='header-btn'><a target="_blank" href="https://drive.google.com/file/d/1z-GyXJ-dYjyFj2dB3dPRKkxFnqA6mqG2/view?usp=share_link" alt="my-resume"><button className='button'>My Resume</button></a></div>
-         </div>
+         </header>
          {/* /////////////////// Header Section End//////////////////////////// */}
+
+
+
+         {/*////////////////// My Skills Section Start/////////////////////////// */}
+         
+
+
+
+
+         {/* //////////////// My Skills Section End ///////////////////////////// */}
     </div>
   );
 }
