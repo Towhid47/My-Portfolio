1b644b548e695a32c4cefe51c2444ed5d88cfcd8
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-12-14 21:39:13 +0600

    Animated Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 
+//////////// Navbar Icons///////
+import { FaHome, FaLightbulb, FaMailBulk, FaProjectDiagram, FaUserAlt } from 'react-icons/fa';
+
+
 function App() {
   return (
     <div className="App">
         {/* ////////////////////// Navbar Section Start///////////////////////// */}
         <div className='navigationbar'>
-           <div>
-               
-           </div>
+              <div>
+                  <a href="#home" alt="home"><FaHome className='nav-item bounce'/></a>
+                  <p>Home</p>
+              </div>
+              <div>
+                  <a href="#projects" alt="projects"><FaProjectDiagram className='nav-item bounce'></FaProjectDiagram></a>
+                  <p>Projects</p>
+              </div>
+              <div>
+                  <a href="#skills" alt="skills"><FaLightbulb className='nav-item bounce'></FaLightbulb></a>
+                  <p>Skills</p>
+              </div>
+              <div>
+                  <a href="#about" alt="about"><FaUserAlt className='nav-item bounce'></FaUserAlt></a>
+                  <p>About Me</p>
+              </div>
+              <div>
+                  <a href="#contact" alt="contact"><FaMailBulk className='nav-item bounce'></FaMailBulk></a>
+                  <p>Contact Me</p>
+              </div>
         </div>
     
 
