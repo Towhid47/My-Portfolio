e884a2bec072dfac0c3ed1eee3aba418e0b4af3b
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-12-15 03:05:01 +0600

    Button Color Change

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ function App() {
             <h1 className='greetings'>Hello! Welcome to my portfolio</h1>
             <h1 className='name'>I'm <span className='my-name'>Towhidur Rahman</span></h1>
             <h1 className='my-designation'><Typewriter options={{  strings: ['MERN Stack Developer', 'UI/UX Designer'],  autoStart: true,  loop: true,  }}/></h1>
+            <h1 className='my-designation'>1+ Years of Experience in Web Development.</h1>
+            
             <div className='social'> <a className='social-link' href="https://www.facebook.com/mdtowhidur.rahman.92"><FaFacebook></FaFacebook></a>{"  "}<a className='social-link' href="https://www.linkedin.com/in/md-towhidur-rahman-83ba78259/"><FaLinkedin></FaLinkedin></a>{"  "} <a className='social-link' href="https://github.com/Towhid47"><FaGithub></FaGithub></a></div>
             <div className='header-btn'><a target="_blank" href="https://drive.google.com/file/d/1z-GyXJ-dYjyFj2dB3dPRKkxFnqA6mqG2/view?usp=share_link" alt="my-resume"><button className='button'>My Resume</button></a></div>
          </header>
