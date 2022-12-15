67d4ac042595ab2d7672bda790b7259bc574438a
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-12-15 21:19:40 +0600

    My Projects Section Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { Button, Card } from 'react-bootstrap';
 //////////// Navbar Icons///////
 import { FaFacebook, FaGithub, FaHome, FaLightbulb, FaLinkedin, FaMailBulk, FaProjectDiagram, FaUserAlt } from 'react-icons/fa';
 
@@ -19,7 +19,12 @@ import tailwind from './assets/skills image/tailwind.png';
 import bootstrap from './assets/skills image/bootstrap.png';
 import mongodb from './assets/skills image/mongodb.png';
 import express from './assets/skills image/expressjs.png';
-import next from './assets/skills image/nextjs.png';
+
+/////////////// Projects Image Import///////////////////
+import divine_travels from './assets/Projects images/divine-travels.jpg';
+import developer_qna from './assets/Projects images/developer-qna.jpg';
+import global_news from './assets/Projects images/global-news.jpg';
+
 
 function App() {
   return (
@@ -97,7 +102,7 @@ function App() {
 
                  <div className='w-50 text-center rotate-img'>
                     <img src={node} alt="" className='w-50'/>
-                    <p className='d-none d-lg-block'>ode JS</p>
+                    <p className='d-none d-lg-block'>Node JS</p>
                  </div>
                  <div className='w-50 text-center rotate-img'>
                     <img src={express} alt="" className='w-50'/>
@@ -122,11 +127,64 @@ function App() {
                  
               </div>
          </section>
-
-
-
-
          {/* //////////////// My Skills Section End ///////////////////////////// */}
+
+
+         {/* /////////////// My Projects Section Start ///////////////////////////*/}
+          <section className='ms-5 mt-5 p-5'>
+            <h1 className='heading text-center mb-5'>My Projects</h1>
+            <div className='cards-container'>
+                {/* ////////////// Divine Travels Project /////////////////// */}
+                <div className='rotate-img'>  
+                    <a href="https://divine-travels-f5721.web.app/" className='text-decoration-none'>
+                        <Card style={{ width: '30rem' }}>
+                            <Card.Img variant="top" src={divine_travels} />
+                            <Card.Body>
+                                <Card.Title className='project-name fs-2'>Divine Travels</Card.Title>
+                                <Card.Text>
+                                    <p className='mt-3 text-light'>This is the Travel Agency Service Website. It is built by using ReactJS</p>
+                                </Card.Text>
+                                <a href="./details"><Button className='button-small w-100'>Details</Button></a>
+                            </Card.Body>
+                        </Card>
+                    </a>
+                </div>
+
+                {/* ////////////// Developer QnA Project /////////////////// */}
+                <div className='rotate-img'>  
+                    <a href="https://developer-qna.netlify.app/" className='text-decoration-none'>
+                        <Card style={{ width: '30rem' }}>
+                            <Card.Img variant="top" src={developer_qna} />
+                            <Card.Body>
+                                <Card.Title className='project-name fs-2'>Developer QnA</Card.Title>
+                                <Card.Text>
+                                    <p className='mt-3 text-light'>This is the Quiz test Website based on the Web Development Topics. It is built by using ReactJS</p>
+                                </Card.Text>
+                                <a href="./details"><Button className='button-small w-100'>Details</Button></a>
+                            </Card.Body>
+                        </Card>
+                    </a>
+                </div>
+
+                {/* ////////////// Global News Project /////////////////// */}
+                <div className='rotate-img'>  
+                    <a href="https://global-news47.netlify.app/" className='text-decoration-none'>
+                        <Card style={{ width: '30rem' }}>
+                            <Card.Img variant="top" src={global_news} />
+                            <Card.Body>
+                                <Card.Title className='project-name fs-2'>Global News</Card.Title>
+                                <Card.Text>
+                                    <p className='mt-3 text-light'>This is the News Portal Website . Built by HTML5 , CSS3 and Javascript</p>
+                                </Card.Text>
+                                <a href="./details"><Button className='button-small w-100'>Details</Button></a>
+                            </Card.Body>
+                        </Card>
+                    </a>
+                </div>
+            </div>
+          </section>
+
+         {/*/////////////// My Projects Section End ///////////////////////////////// */}
     </div>
   );
 }
